2b87b2025aebfb2feb26980cd6ddfbdb8e6f61b9
Jaredh01 <jaredh01@comcast.net>
2019-02-22 20:54:23 -0600

    Initial Solution

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -34,7 +34,20 @@ class TowerOfHanoiGame(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### student code goes here
-        pass
+        result = []
+        for i in range(1,4):
+            onBindings = self.kb.kb_ask(parse_input('fact: (on ?disk peg' + str(i) + ')'))
+            disksOnPeg = []
+            if onBindings == False: 
+                result.append(())
+                continue
+            for bind in onBindings:
+                disksOnPeg.append(int(bind.bindings[0].constant.element[-1:]))
+            disksOnPeg.sort()
+            result.append(tuple(disksOnPeg))
+        return tuple(result)
+
+
 
     def makeMove(self, movable_statement):
         """
@@ -53,7 +66,33 @@ class TowerOfHanoiGame(GameMaster):
             None
         """
         ### Student code goes here
-        pass
+        pred = movable_statement.predicate
+        sl = movable_statement.terms
+        self.kb.kb_retract(Fact(Statement(["on", sl[0],sl[1]])))
+        self.kb.kb_retract(Fact(Statement(["topof", sl[0], sl[1]])))
+        potentialNewTop = self.kb.kb_ask(parse_input('fact: (ontopof ' + sl[0].term.element + " ?disk)"))
+        #Determine if inital peg is now empty or has a new top
+        if potentialNewTop != False:
+            potentialNewTop = potentialNewTop[0].bindings[0].constant.element
+            self.kb.kb_retract(Fact(Statement(["ontopof",sl[0],potentialNewTop])))
+            self.kb.kb_assert(Fact(Statement(["topof", potentialNewTop, sl[1]])))
+        else: 
+            self.kb.kb_assert(Fact(Statement(["empty",sl[1]])))
+        #Determine if the target peg is empty or already has disks
+        if self.kb.kb_ask(parse_input('fact: (empty ' + sl[2].term.element + ')')) == False:
+            oldTop = self.kb.kb_ask(parse_input('fact: (topof ?disk ' + sl[2].term.element + ')'))
+            oldTop = oldTop[0].bindings[0].constant.element
+            self.kb.kb_retract(Fact(Statement(["topof", oldTop, sl[2]])))
+            self.kb.kb_assert(Fact(Statement(["ontopof", sl[0], oldTop])))
+        else: 
+            self.kb.kb_retract(Fact(Statement(["empty",sl[2]])))
+        
+        self.kb.kb_assert(Fact(Statement(["on",sl[0],sl[2]])))
+        self.kb.kb_assert(Fact(Statement(["topof",sl[0],sl[2]])))
+        
+
+
+
 
     def reverseMove(self, movable_statement):
         """
@@ -100,7 +139,22 @@ class Puzzle8Game(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### Student code goes here
-        pass
+        result = []
+        for i in range(1,4):
+            tilesInRow = []
+            for j in range(1,4):
+                #breakpoint()
+                tile = self.kb.kb_ask(parse_input('fact: (at ?tile pos' + str(j) + ' pos' + str(i) + ')'))
+                tile = tile[0].bindings[0].constant.element
+                if (tile == 'empty'):
+                    tilesInRow.append(-1)
+                else:
+                    tilesInRow.append(int(tile[-1:]))
+            result.append(tuple(tilesInRow))
+
+        return tuple(result)
+                
+
 
     def makeMove(self, movable_statement):
         """
@@ -119,7 +173,13 @@ class Puzzle8Game(GameMaster):
             None
         """
         ### Student code goes here
-        pass
+        pred = movable_statement.predicate
+        sl = movable_statement.terms
+        self.kb.kb_retract(Fact(Statement(["at",sl[0],sl[1],sl[2]])))
+        self.kb.kb_retract(Fact(Statement(["at","empty",sl[3],sl[4]])))
+        self.kb.kb_assert(Fact(Statement(["at",sl[0],sl[3],sl[4]])))
+        self.kb.kb_assert(Fact(Statement(["at","empty",sl[1],sl[2]])))
+
 
     def reverseMove(self, movable_statement):
         """
--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -1,5 +1,6 @@
 
 from solver import *
+from Lib import queue
 
 class SolverDFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
@@ -18,13 +19,45 @@ class SolverDFS(UninformedSolver):
         Returns:
             True if the desired solution state is reached, False otherwise
         """
-        ### Student code goes here
-        return True
+        ### Student code goes her
+        
+        if self.currentState.state == self.victoryCondition: return True
+        self.visited[self.currentState] = True
+        nextMoves = self.gm.getMovables()
+        if nextMoves == False:
+            self.gm.reverseMove(self.currentState.requiredMovable)
+            self.currentState = self.currentState.parent
+            return False
+        for move in nextMoves:
+            self.gm.makeMove(move)
+            childState = GameState(self.gm.getGameState(),0,move)
+            if childState in self.visited: 
+                self.gm.reverseMove(move)
+                continue # can just continue, cause we should never revisit a child node
+            childState.parent = self.currentState
+            self.currentState = childState
+            return False
+        
+
 
 
 class SolverBFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
         super().__init__(gameMaster, victoryCondition)
+        self.visitQueue = queue.Queue()
+        #Assemble a list of the children of the root
+        nextMoves = self.gm.getMovables()
+        for move in nextMoves:
+            self.gm.makeMove(move)
+            childState = GameState(self.gm.getGameState(), self.currentState.depth + 1, move)
+            if childState.state == self.victoryCondition: return True
+            if childState in self.visited: 
+                self.gm.reverseMove(move)
+                continue # can just continue, cause we should never revisit a child node
+            self.visited[childState] = True
+            childState.parent = self.currentState
+            self.visitQueue.put(childState)
+            self.gm.reverseMove(move)
 
     def solveOneStep(self):
         """
@@ -40,4 +73,49 @@ class SolverBFS(UninformedSolver):
             True if the desired solution state is reached, False otherwise
         """
         ### Student code goes here
-        return True
+        
+        #keep a FIFO queue
+        #Until at desired nodes / at root, back up the tree
+        #If reach root, construct a chain of moves from node to root
+        #descend from parent to new node
+        #At node, generate children, check if each is victory / already visited
+        #If not, then add child to queue
+
+        #Get to the next node to visit
+        stateToVisit = self.visitQueue.get()
+        movesFromTarget = []
+        targetBacktrackNode = stateToVisit
+        while self.currentState.depth != 0 and self.currentState != targetBacktrackNode:
+            self.gm.reverseMove(self.currentState.requiredMovable)
+            self.currentState = self.currentState.parent
+            movesFromTarget.append(targetBacktrackNode.requiredMovable)
+            targetBacktrackNode = targetBacktrackNode.parent
+
+        if self.currentState == targetBacktrackNode:
+            while movesFromTarget != []:
+                self.gm.makeMove(movesFromTarget.pop())
+            self.currentState = stateToVisit
+
+        if self.currentState.depth == 0 or self.currentState != stateToVisit:
+            while self.currentState != targetBacktrackNode:
+                movesFromTarget.append(targetBacktrackNode.requiredMovable)
+                targetBacktrackNode = targetBacktrackNode.parent
+            while movesFromTarget != []:
+                self.gm.makeMove(movesFromTarget.pop())
+            self.currentState = stateToVisit
+        
+        #At this point, we should be at the desired state
+        #Check if children are winners / already visited
+        #if not, add them to the queue
+        nextMoves = self.gm.getMovables()
+        for move in nextMoves:
+            self.gm.makeMove(move)
+            childState = GameState(self.gm.getGameState(), self.currentState.depth + 1, move)
+            if childState.state == self.victoryCondition: return True
+            if childState in self.visited: 
+                self.gm.reverseMove(move)
+                continue # can just continue, cause we should never revisit a child node
+            self.visited[childState] = True
+            childState.parent = self.currentState
+            self.visitQueue.put(childState)
+            self.gm.reverseMove(move)
